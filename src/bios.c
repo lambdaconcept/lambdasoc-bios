b6bc1384b8e8c8e1f2b4adb4e7f69cfc216da179
Jean-François Nguyen <jf@lambdaconcept.com>
2021-06-04 16:01:02 +0200

    Add memory read/write/copy commands.
    Taken from MiSoC/LiteX.

--- src/bios.c
+++ src/bios.c
@@ -5,13 +5,186 @@
 #include <stdio.h>
 #include <string.h>
 #include <stddef.h>
+#include <stdlib.h>
 #include <uart.h>
+
+#include <boot/common.h>
+
+#if CONFIG_WITH_SDRAM
+# include <sdram.h>
+#endif
+
+/* General address space functions */
+
+#define NUMBER_OF_BYTES_ON_A_LINE 16
+static void dump_bytes(unsigned int *ptr, int count, unsigned addr)
+{
+	char *data = (char *)ptr;
+	int line_bytes = 0, i = 0;
+
+	putsnonl("Memory dump:");
+	while(count > 0){
+		line_bytes =
+			(count > NUMBER_OF_BYTES_ON_A_LINE)?
+				NUMBER_OF_BYTES_ON_A_LINE : count;
+
+		printf("\n0x%08x  ", addr);
+		for(i=0;i<line_bytes;i++)
+			printf("%02x ", *(unsigned char *)(data+i));
+
+		for(;i<NUMBER_OF_BYTES_ON_A_LINE;i++)
+			printf("   ");
+
+		printf(" ");
+
+		for(i=0;i<line_bytes;i++) {
+			if((*(data+i) < 0x20) || (*(data+i) > 0x7e))
+				printf(".");
+			else
+				printf("%c", *(data+i));
+		}
+
+		for(;i<NUMBER_OF_BYTES_ON_A_LINE;i++)
+			printf(" ");
+
+		data += (char)line_bytes;
+		count -= line_bytes;
+		addr += line_bytes;
+	}
+	printf("\n");
+}
+
+static void mr(char *startaddr, char *len)
+{
+	char *c;
+	unsigned int *addr;
+	unsigned int length;
+
+	if(*startaddr == 0) {
+		printf("mr <address> [length]\n");
+		return;
+	}
+	addr = (unsigned *)strtoul(startaddr, &c, 0);
+	if(*c != 0) {
+		printf("incorrect address\n");
+		return;
+	}
+	if(*len == 0) {
+		length = 4;
+	} else {
+		length = strtoul(len, &c, 0);
+		if(*c != 0) {
+			printf("incorrect length\n");
+			return;
+		}
+	}
+
+	dump_bytes(addr, length, (unsigned)addr);
+}
+
+static void mw(char *addr, char *value, char *count)
+{
+	char *c;
+	unsigned int *addr2;
+	unsigned int value2;
+	unsigned int count2;
+	unsigned int i;
+
+	if((*addr == 0) || (*value == 0)) {
+		printf("mw <address> <value> [count]\n");
+		return;
+	}
+	addr2 = (unsigned int *)strtoul(addr, &c, 0);
+	if(*c != 0) {
+		printf("incorrect address\n");
+		return;
+	}
+	value2 = strtoul(value, &c, 0);
+	if(*c != 0) {
+		printf("incorrect value\n");
+		return;
+	}
+	if(*count == 0) {
+		count2 = 1;
+	} else {
+		count2 = strtoul(count, &c, 0);
+		if(*c != 0) {
+			printf("incorrect count\n");
+			return;
+		}
+	}
+	for (i=0;i<count2;i++) *addr2++ = value2;
+}
+
+static void mc(char *dstaddr, char *srcaddr, char *count)
+{
+	char *c;
+	unsigned int *dstaddr2;
+	unsigned int *srcaddr2;
+	unsigned int count2;
+	unsigned int i;
+
+	if((*dstaddr == 0) || (*srcaddr == 0)) {
+		printf("mc <dst> <src> [count]\n");
+		return;
+	}
+	dstaddr2 = (unsigned int *)strtoul(dstaddr, &c, 0);
+	if(*c != 0) {
+		printf("incorrect destination address\n");
+		return;
+	}
+	srcaddr2 = (unsigned int *)strtoul(srcaddr, &c, 0);
+	if(*c != 0) {
+		printf("incorrect source address\n");
+		return;
+	}
+	if(*count == 0) {
+		count2 = 1;
+	} else {
+		count2 = strtoul(count, &c, 0);
+		if(*c != 0) {
+			printf("incorrect count\n");
+			return;
+		}
+	}
+	for (i=0;i<count2;i++) *dstaddr2++ = *srcaddr2++;
+}
+
+static void crc(char *startaddr, char *len)
+{
+	char *c;
+	char *addr;
+	unsigned int length;
+
+	if((*startaddr == 0)||(*len == 0)) {
+		printf("crc <address> <length>\n");
+		return;
+	}
+	addr = (char *)strtoul(startaddr, &c, 0);
+	if(*c != 0) {
+		printf("incorrect address\n");
+		return;
+	}
+	length = strtoul(len, &c, 0);
+	if(*c != 0) {
+		printf("incorrect length\n");
+		return;
+	}
+
+	printf("CRC32: %08x\n", crc32((unsigned char *)addr, length));
+}
+
+/* Init + command line */
 
 static void help(void)
 {
 	puts("LambdaSoC BIOS");
 	puts("Available commands:");
 	puts("help       - show this message");
+	puts("mr         - read address space");
+	puts("mw         - write address space");
+	puts("mc         - copy address space");
+	puts("crc        - compute CRC32 of a part of the address space");
 	puts("serialboot - boot via SFL");
 }
 
@@ -37,6 +210,14 @@ static void do_command(char *c)
 
 	if (strcmp(token, "help") == 0)
 		help();
+        else if (strcmp(token, "mr") == 0)
+            mr(get_token(&c), get_token(&c));
+        else if (strcmp(token, "mw") == 0)
+            mw(get_token(&c), get_token(&c), get_token(&c));
+        else if (strcmp(token, "mc") == 0)
+            mc(get_token(&c), get_token(&c), get_token(&c));
+        else if (strcmp(token, "crc") == 0)
+            crc(get_token(&c), get_token(&c));
 	else if (strcmp(token, "serialboot") == 0)
 		serialboot();
 	else if (strcmp(token, "") != 0)
